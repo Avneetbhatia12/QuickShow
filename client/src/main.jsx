5dd57cee521e1d9e6b2abc5085f3d0d0694b1252
Avneetbhatia12 <avu00bhatia@gmail.com>
2026-03-22 22:49:42 +0530

    Added basic structure and clerk auth

---
+++ client/src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import { ClerkProvider } from "@clerk/react";
+
+createRoot(document.getElementById("root")).render(
+  <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>,
+);
