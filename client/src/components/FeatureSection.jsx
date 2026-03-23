f90a487233043738ee910a3bb53bcd6de55a27fe
Avneetbhatia12 <avu00bhatia@gmail.com>
2026-03-23 13:15:53 +0530

    Added movie card component

--- client/src/components/FeatureSection.jsx
+++ client/src/components/FeatureSection.jsx
@@ -2,6 +2,8 @@ import { ArrowRight } from 'lucide-react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import BlurCircle from './BlurCircle'
+import MovieCard from './MovieCard'
+import { dummyShowsData } from '../assets/assets'
 
 const FeatureSection = () => {
     const navigate = useNavigate()
@@ -15,7 +17,9 @@ const FeatureSection = () => {
             View All <ArrowRight className=' group-hover:translate-x-0.5 transition w-4.5 h-4.5 '/>
         </button>
       </div>
-    
+        <div className='flex flex-wrap max-sm:justify-center gap-8 mt-8'>
+        {dummyShowsData.slice(0,8).map((show)=>(<MovieCard key={show._id} movie={show}/>))}
+        </div>
       <div className='flex justify-center mt-20'>
         <button onClick={()=> {navigate('/movies'); scrollTo(0,0)}}
             className='px-10 py-3 text-sm bg-red-400 hover:bg-red-500 transition rounded-md font-medium cursor-pointer'>
